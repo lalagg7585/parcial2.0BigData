85258cc4435ef7605b7dcc7a9d2693905f8b1919
Laura <laura.garcia07@usa.edu.co>
2024-04-26 14:46:21 +0000

    Pruebas unitarias 2

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import unittest
-from unittest.mock import MagicMock
-from lambda_function import lambda_handler  # Asegúrate de importar tu función lambda_handler desde tu archivo lambda_function.py
+from unittest.mock import MagicMock, patch
+from funcion_lambda import lambda_handler  # Asegúrate de importar tu función lambda_handler desde tu archivo lambda_function.py
 
 class TestLambdaHandler(unittest.TestCase):
     
-    def test_lambda_handler(self):
+    @patch('funcion_lambda.boto3.client')
+    def test_lambda_handler(self, mock_client):
         # Creamos un evento de prueba y un contexto de prueba
         event = {}
         context = MagicMock()
-
+    
         # Llamamos a la función lambda_handler
         response = lambda_handler(event, context)
-
+    
         # Verificamos que se haya llamado a boto3.client con los argumentos correctos
-        context.client.assert_called_once_with('emr', region_name='us-east-1')
+        mock_client.assert_called_once_with('emr', region_name='us-east-1')
 
         # Verificamos que la respuesta sea la esperada
         self.assertEqual(response['statusCode'], 200)
